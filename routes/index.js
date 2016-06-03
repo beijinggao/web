edad97c8feaec658e5db6f76077dfa5c2fc81b9b
gao8954 <895407214gao@sina.com>
2016-06-03 18:36:19 +0800

    add page

--- routes/index.js
+++ routes/index.js
@@ -12,22 +12,32 @@ router.get('/', function (req, res, next) {
     cookie.redirect_js(req, res, next)
     //res.render('index', {title: 'Express'});
 });
-router.get('/blog.html', function(req, res, next) {
-    var url="http://www.qiushibaike.com/8hr/page/"+req.query.page+"/?s=4882864";
-    var data= new ArrayList;
+router.get('/blog.html', function (req, res, next) {
+    var url = "http://www.qiushibaike.com/8hr/page/" + req.query.page + "/?s=4882864";
+    var data = new ArrayList;
     jsdom.env({
         url: url,
         scripts: ["http://code.jquery.com/jquery.js"],
         done: function (errors, window) {
             var $ = window.$;
-            $('.content').each(function(index,value){
-                data.set(index,$(value).text());
+            $('.article').each(function (index, value) {
+                console.log($(this).find('.thumb img').attr('src'));
+                var content = $(value).find('.content').text();
+                var img = $(value).find('.thumb img').attr('src');
+                if(img !=undefined){
+                    data.set(index, [content, img]);
+                }else{
+                    data.set(index, [content]);
+                }
             });
+            /*$('.content').each(function (index, value) {
+                data.set(index, $(value).text());
+            });*/
             res.render('blog', {title: data});
         }
     });
 });
-router.get('/index.html', function(req, res, next) {
+router.get('/index.html', function (req, res, next) {
     res.render('index', {title: 'aaaaaaaaaaaaaaaaaa'});
     res.send('respond with a resource');
 });
